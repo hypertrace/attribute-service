019ab290d9c315cf61f559866d36e2cfa3f5a72f
Aaron Steinfeld <aaron@traceable.ai>
2020-11-23 16:38:01 -0500

    fix: add support for projecting to and from null values

--- attribute-projection-registry/src/main/java/org/hypertrace/core/attribute/service/projection/AbstractAttributeProjection.java
+++ attribute-projection-registry/src/main/java/org/hypertrace/core/attribute/service/projection/AbstractAttributeProjection.java
@@ -3,16 +3,15 @@ package org.hypertrace.core.attribute.service.projection;
 import com.google.common.base.Preconditions;
 import java.util.ArrayList;
 import java.util.List;
-import org.hypertrace.core.attribute.service.v1.AttributeKind;
 import org.hypertrace.core.attribute.service.v1.LiteralValue;
 
 abstract class AbstractAttributeProjection<R> implements AttributeProjection {
 
-  private final AttributeKind resultKind;
-  private final List<AttributeKind> argumentKinds;
+  private final AttributeKindWithNullability resultKind;
+  private final List<AttributeKindWithNullability> argumentKinds;
 
   protected AbstractAttributeProjection(
-      AttributeKind resultKind, List<AttributeKind> argumentKinds) {
+      AttributeKindWithNullability resultKind, List<AttributeKindWithNullability> argumentKinds) {
     this.resultKind = resultKind;
     this.argumentKinds = argumentKinds;
   }
@@ -24,26 +23,39 @@ abstract class AbstractAttributeProjection<R> implements AttributeProjection {
     for (int index = 0; index < arguments.size(); index++) {
       int argumentIndex = index;
       LiteralValue argumentLiteral = arguments.get(argumentIndex);
-      AttributeKind attributeKind = this.argumentKinds.get(argumentIndex);
+      AttributeKindWithNullability maybeNullableKind = this.argumentKinds.get(argumentIndex);
       Object unwrappedArgument =
-          ValueCoercer.fromLiteral(argumentLiteral, attributeKind)
-              .orElseThrow(
-                  () ->
-                      new IllegalArgumentException(
-                          String.format(
-                              "Projection argument %s at index %d could not be converted to expected type %s",
-                              argumentLiteral, argumentIndex, attributeKind)));
+          ValueCoercer.fromLiteral(argumentLiteral, maybeNullableKind.getKind())
+              .orElseGet(
+                  () -> {
+                    if (maybeNullableKind.isNullable()) {
+                      return null;
+                    }
+                    throw new IllegalArgumentException(
+                        String.format(
+                            "Projection argument %s at index %d could not be converted to expected type %s",
+                            argumentLiteral, argumentIndex, maybeNullableKind));
+                  });
 
       unwrappedArguments.add(argumentIndex, unwrappedArgument);
     }
     Object unwrappedResult = this.doUnwrappedProjection(unwrappedArguments);
-    return ValueCoercer.toLiteral(unwrappedResult, this.resultKind)
+    return ValueCoercer.toLiteral(
+            unwrappedResult, this.resultKind.getKind(), this.resultKind.isNullable())
         .orElseThrow(
             () ->
                 new UnsupportedOperationException(
                     String.format(
                         "Projection result %s could not be converted to expected type %s",
                         unwrappedResult, this.resultKind)));
+  }
+
+  private <T> T nullOrThrow(
+      AttributeKindWithNullability maybeNullableKind, RuntimeException exception) {
+    if (maybeNullableKind.isNullable()) {
+      return null;
+    }
+    throw exception;
   }
 
   protected abstract R doUnwrappedProjection(List<Object> arguments);
