92caa67eb6a9a97098af0698d69dd9880f1f02f4
Aaron Steinfeld <aaron@traceable.ai>
2020-11-24 14:16:39 -0500

    refactor: expose projection data types

--- attribute-projection-registry/src/main/java/org/hypertrace/core/attribute/service/projection/AbstractAttributeProjection.java
+++ attribute-projection-registry/src/main/java/org/hypertrace/core/attribute/service/projection/AbstractAttributeProjection.java
@@ -3,27 +3,30 @@ package org.hypertrace.core.attribute.service.projection;
 import com.google.common.base.Preconditions;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import org.hypertrace.core.attribute.service.v1.AttributeKind;
 import org.hypertrace.core.attribute.service.v1.LiteralValue;
 
 abstract class AbstractAttributeProjection<R> implements AttributeProjection {
 
-  private final AttributeKindWithNullability resultKind;
-  private final List<AttributeKindWithNullability> argumentKinds;
+  private final AttributeKindWithNullability resultKindWithNullability;
+  private final List<AttributeKindWithNullability> argumentKindsWithNullability;
 
   protected AbstractAttributeProjection(
       AttributeKindWithNullability resultKind, List<AttributeKindWithNullability> argumentKinds) {
-    this.resultKind = resultKind;
-    this.argumentKinds = argumentKinds;
+    this.resultKindWithNullability = resultKind;
+    this.argumentKindsWithNullability = argumentKinds;
   }
 
   @Override
   public LiteralValue project(List<LiteralValue> arguments) {
-    Preconditions.checkArgument(arguments.size() == argumentKinds.size());
-    List<Object> unwrappedArguments = new ArrayList<>(argumentKinds.size());
+    Preconditions.checkArgument(arguments.size() == this.argumentKindsWithNullability.size());
+    List<Object> unwrappedArguments = new ArrayList<>(this.argumentKindsWithNullability.size());
     for (int index = 0; index < arguments.size(); index++) {
       int argumentIndex = index;
       LiteralValue argumentLiteral = arguments.get(argumentIndex);
-      AttributeKindWithNullability maybeNullableKind = this.argumentKinds.get(argumentIndex);
+      AttributeKindWithNullability maybeNullableKind =
+          this.argumentKindsWithNullability.get(argumentIndex);
       Object unwrappedArgument =
           ValueCoercer.fromLiteral(argumentLiteral, maybeNullableKind.getKind())
               .orElseGet(
@@ -41,13 +44,27 @@ abstract class AbstractAttributeProjection<R> implements AttributeProjection {
     }
     Object unwrappedResult = this.doUnwrappedProjection(unwrappedArguments);
     return ValueCoercer.toLiteral(
-            unwrappedResult, this.resultKind.getKind(), this.resultKind.isNullable())
+            unwrappedResult,
+            this.resultKindWithNullability.getKind(),
+            this.resultKindWithNullability.isNullable())
         .orElseThrow(
             () ->
                 new UnsupportedOperationException(
                     String.format(
                         "Projection result %s could not be converted to expected type %s",
-                        unwrappedResult, this.resultKind)));
+                        unwrappedResult, this.resultKindWithNullability)));
+  }
+
+  @Override
+  public AttributeKind getResultKind() {
+    return this.resultKindWithNullability.getKind();
+  }
+
+  @Override
+  public List<AttributeKind> getArgumentKinds() {
+    return this.argumentKindsWithNullability.stream()
+        .map(AttributeKindWithNullability::getKind)
+        .collect(Collectors.toUnmodifiableList());
   }
 
   protected abstract R doUnwrappedProjection(List<Object> arguments);
