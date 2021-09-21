61b69b9f374db49d0d6011119279dd276bdc5b78
Ankit Choudhary <ankitchoudhary@192.168.2.103>
2021-09-21 16:39:56 +0530

    fix3

--- attribute-service/build.gradle.kts
+++ attribute-service/build.gradle.kts
@@ -75,9 +75,6 @@ dependencies {
     runtimeOnly("io.netty:netty-handler-proxy:4.1.68.Final") {
       because("https://snyk.io/vuln/SNYK-JAVA-IONETTY-1089809")
     }
-    runtimeOnly("io.netty:netty-codec-http2@4.1.68.Final") {
-      because("https://snyk.io/vuln/SNYK-JAVA-IONETTY-1042268")
-    }
   }
 
   // Config
