3d6b95955df0da281b0f924e93d72aaf520a7980
Aaron Steinfeld <aaron@traceable.ai>
2021-12-10 09:53:24 -0500

    fix: upgrade vulnerable log4j

--- caching-attribute-service-client/build.gradle.kts
+++ caching-attribute-service-client/build.gradle.kts
@@ -23,7 +23,7 @@ dependencies {
   testImplementation("org.mockito:mockito-core:3.8.0")
   testImplementation("org.mockito:mockito-junit-jupiter:3.8.0")
   testImplementation("io.grpc:grpc-core:1.42.0")
-  testImplementation("org.apache.logging.log4j:log4j-slf4j-impl:2.14.1")
+  testImplementation("org.apache.logging.log4j:log4j-slf4j-impl:2.15.0")
 }
 
 tasks.test {
