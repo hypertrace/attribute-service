a6ce696629623c54b2823ff82daecfc0db3f641a
SJ <shubhamjindal18@gmail.com>
2022-04-04 15:31:58 +0530

    chore: upgrade dependencies

--- caching-attribute-service-client/build.gradle.kts
+++ caching-attribute-service-client/build.gradle.kts
@@ -11,9 +11,9 @@ dependencies {
   api("io.grpc:grpc-api")
 
   implementation("io.grpc:grpc-stub")
-  implementation("org.hypertrace.core.grpcutils:grpc-client-utils:0.7.0")
-  implementation("org.hypertrace.core.grpcutils:grpc-client-rx-utils:0.7.0")
-  implementation("org.hypertrace.core.grpcutils:grpc-context-utils:0.7.0")
+  implementation("org.hypertrace.core.grpcutils:grpc-client-utils:0.7.2")
+  implementation("org.hypertrace.core.grpcutils:grpc-client-rx-utils:0.7.2")
+  implementation("org.hypertrace.core.grpcutils:grpc-context-utils:0.7.2")
   implementation("com.google.guava:guava:31.0.1-jre")
   annotationProcessor("org.projectlombok:lombok:1.18.22")
   compileOnly("org.projectlombok:lombok:1.18.22")
