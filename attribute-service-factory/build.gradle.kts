97898b6b7882ba8f038439a528a3cc9aee5dbbc0
Aaron Steinfeld <45047841+aaron-steinfeld@users.noreply.github.com>
2022-07-06 15:53:46 +0200

    chore: update service framework version (#130)

--- attribute-service-factory/build.gradle.kts
+++ attribute-service-factory/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 dependencies {
-  api("org.hypertrace.core.serviceframework:platform-grpc-service-framework:0.1.35")
+  api("org.hypertrace.core.serviceframework:platform-grpc-service-framework:0.1.36")
 
   // Only required because AttributeService constructor test overload uses a doc store API
   compileOnly("org.hypertrace.core.documentstore:document-store:0.6.15")
