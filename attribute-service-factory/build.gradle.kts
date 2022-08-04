8e94c4602201d912ebd40bd4deca2d63dc6db360
Ronak <ronak@traceable.ai>
2022-08-04 22:17:23 +0530

    chore: upgradting doc-store libs

--- attribute-service-factory/build.gradle.kts
+++ attribute-service-factory/build.gradle.kts
@@ -6,7 +6,7 @@ dependencies {
   api("org.hypertrace.core.serviceframework:platform-grpc-service-framework:0.1.37")
 
   // Only required because AttributeService constructor test overload uses a doc store API
-  compileOnly("org.hypertrace.core.documentstore:document-store:0.6.15")
+  compileOnly("org.hypertrace.core.documentstore:document-store:0.7.1")
 
   implementation(project(":attribute-service-impl"))
 }
