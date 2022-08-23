2e0c479f33eee551d9e13025fdfa366cbe37467d
SJ <shubhamjindal18@gmail.com>
2022-08-23 11:21:09 +0530

    chore: upgrade uuid

--- attribute-projection-functions/build.gradle.kts
+++ attribute-projection-functions/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 dependencies {
   api("com.google.code.findbugs:jsr305:3.0.2")
-  implementation("com.github.f4b6a3:uuid-creator:3.5.0")
+  implementation("com.github.f4b6a3:uuid-creator:5.1.0")
 
   testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
 }
--- attribute-projection-functions/src/main/java/org/hypertrace/core/attribute/service/projection/functions/Hash.java
+++ attribute-projection-functions/src/main/java/org/hypertrace/core/attribute/service/projection/functions/Hash.java
@@ -3,7 +3,6 @@ package org.hypertrace.core.attribute.service.projection.functions;
 import static java.util.Objects.nonNull;
 
 import com.github.f4b6a3.uuid.UuidCreator;
-import com.github.f4b6a3.uuid.creator.rfc4122.NameBasedSha1UuidCreator;
 import java.util.UUID;
 import javax.annotation.Nullable;
 
@@ -14,12 +13,11 @@ public class Hash {
    * would change any existing projections containing a hash and orphan any data persisted against
    * that such as stored entities.
    */
-  private static final NameBasedSha1UuidCreator HASHER =
-      UuidCreator.getNameBasedSha1Creator()
-          .withNamespace(UUID.fromString("5088c92d-5e9c-43f4-a35b-2589474d5642"));
+  private static final UUID NAMESPACE_UUID =
+      UUID.fromString("5088c92d-5e9c-43f4-a35b-2589474d5642");
 
   @Nullable
   public static String hash(@Nullable String value) {
-    return nonNull(value) ? HASHER.create(value).toString() : null;
+    return nonNull(value) ? UuidCreator.getNameBasedSha1(NAMESPACE_UUID, value).toString() : null;
   }
 }
