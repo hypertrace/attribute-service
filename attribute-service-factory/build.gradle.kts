1861f394233f5431f81e6de0598e66d2dbad5ebc
SrikarMannepalli <37926341+SrikarMannepalli@users.noreply.github.com>
2022-09-19 21:20:14 +0530

    chore: add DISTINCT_ARRAY aggregate function (#145)
    * add DISTINCT_ARRAY aggregate function
    
    * address review comments
    
    * update document store

--- attribute-service-factory/build.gradle.kts
+++ attribute-service-factory/build.gradle.kts
@@ -6,7 +6,7 @@ dependencies {
   api("org.hypertrace.core.serviceframework:platform-grpc-service-framework:0.1.37")
 
   // Only required because AttributeService constructor test overload uses a doc store API
-  compileOnly("org.hypertrace.core.documentstore:document-store:0.7.8")
+  compileOnly("org.hypertrace.core.documentstore:document-store:0.7.10")
 
   implementation(project(":attribute-service-impl"))
 }
