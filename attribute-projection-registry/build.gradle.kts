78ae89c426c28c24f316a607e7a494155d593029
Aaron Steinfeld <aaron@traceable.ai>
2023-08-25 20:26:13 -0400

    chore: update locks

--- attribute-projection-registry/build.gradle.kts
+++ attribute-projection-registry/build.gradle.kts
@@ -21,7 +21,7 @@ dependencyLocking {
 tasks.register("resolveAndLockAll") {
   notCompatibleWithConfigurationCache("Filters configurations at execution time")
   doFirst {
-    require(gradle.startParameter.isWriteDependencyLocks) { "${path} must be run from the command line with the `--write-locks` flag" }
+    require(gradle.startParameter.isWriteDependencyLocks) { "$path must be run from the command line with the `--write-locks` flag" }
   }
   doLast {
     configurations.filter {
